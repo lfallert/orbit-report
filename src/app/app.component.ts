7dc63ece598a78155a95f02ab8386913c5a9212c
LFallert <lfallert13@yahoo.com>
2021-07-01 16:53:48 -0500

    finished sort, still need fetch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,5 +24,22 @@ export class AppComponent {
     ];
  }
 
+  // constructor() {
+  //   this.sourceList = [];
+  //   let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+  //   window.fetch(satellitesUrl).then(function(response) {
+  //     response.json().then(function(data) {
+
+  //         let fetchedSatellites = data.satellites;
+  //         // TODO: loop over satellites
+  //         // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+  //         // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
+
+  //     }.bind(this));
+  //   }.bind(this));
+
+  // }
+
 }
 
